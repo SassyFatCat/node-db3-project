1ed6fc017544379e1da009ab8de8cb9ef68bb813
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-10 16:26:24 -0500

    saving

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,10 @@ const db = knex(config["development"]);
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps,
+    add,
+    update
 };
 
 function find() {
@@ -14,4 +17,28 @@ function find() {
 function findById(id) {
     return db('schemes')
         .where({ id })
+}
+
+function findSteps(id) {
+    return db.select(['step_number', 'instructions', 'schemes.scheme_name']).from('steps')
+        .join('schemes', 'scheme_id', 'schemes.id')
+        .where({ scheme_id: id })
+        .orderBy('step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(id => {
+            return findById(id[0])
+        })
+}
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+            .then(response => {
+                return findById(id)
+            })
 }
